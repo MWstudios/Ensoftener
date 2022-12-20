85f105b25dcd7895c72e98a1160a2b196b276395
MWstudios <45273040+MWstudios@users.noreply.github.com>
2022-12-20 15:30:11 +0100

    Delete EnsoftenerCpp directory

--- EnsoftenerCpp/EnsCpp.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "EnsCpp.h"
-
-
-HRESULT RegisterEffectFromString(ID2D1Factory1* factory, GUID guid, PCWSTR string, D2D1_PROPERTY_BINDING* bindings, UINT bindingLength, PD2D1_EFFECT_FACTORY eFactory)
-{ return factory->RegisterEffectFromString(guid, string, bindings, bindingLength, eFactory); }
-
-UINT InteropTest() { return 42; }
