9d428b27a4aac6e6ba4eeace6101c8820dc418dc
MWstudios <mwstudios@users.noreply.github.com>
2022-12-20 15:27:19 +0100

    reverted DCChaining changes

--- src/DCChaining.cs
+++ src/DCChaining.cs
@@ -10,49 +10,48 @@ namespace Ensoftener
     public static class DCChaining
     {
         public static DeviceContext ChainBeginDraw(this DeviceContext d2dc) { d2dc.BeginDraw(); return d2dc; }
-        public static RenderTarget ChainBeginDraw(this RenderTarget d2dc) { d2dc.BeginDraw(); return d2dc; }
-        public static RenderTarget ChainClear(this RenderTarget d2dc, RawColor4? clearColor) { d2dc.Clear(clearColor); return d2dc; }
+        public static DeviceContext ChainClear(this DeviceContext d2dc, RawColor4? clearColor) { d2dc.Clear(clearColor); return d2dc; }
         public static DeviceContext ChainDrawBitmap(this DeviceContext d2dc, Bitmap bitmap,
             float opacity = 1, InterpolationMode interpolationMode = InterpolationMode.NearestNeighbor)
         { d2dc.DrawBitmap(bitmap, opacity, interpolationMode); return d2dc; }
-        public static RenderTarget ChainDrawBitmap(this RenderTarget d2dc, Bitmap bitmap, RawRectangleF? sourceRectangle,
+        public static DeviceContext ChainDrawBitmap(this DeviceContext d2dc, Bitmap bitmap, RawRectangleF? sourceRectangle,
             RawRectangleF? destinationRectangle, float opacity = 1, BitmapInterpolationMode interpolationMode = BitmapInterpolationMode.NearestNeighbor)
         { d2dc.DrawBitmap(bitmap, destinationRectangle, opacity, interpolationMode, sourceRectangle); return d2dc; }
-        public static RenderTarget ChainDrawEllipse(this RenderTarget d2dc, Ellipse ellipse, Brush brush, float strokeWidth = 1)
+        public static DeviceContext ChainDrawEllipse(this DeviceContext d2dc, Ellipse ellipse, Brush brush, float strokeWidth = 1)
         { d2dc.DrawEllipse(ellipse, brush, strokeWidth); return d2dc; }
-        public static RenderTarget ChainDrawEllipse(this RenderTarget d2dc, Ellipse ellipse, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
+        public static DeviceContext ChainDrawEllipse(this DeviceContext d2dc, Ellipse ellipse, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
         { d2dc.DrawEllipse(ellipse, brush, strokeWidth, strokeStyle); return d2dc; }
-        public static RenderTarget ChainDrawGeometry(this RenderTarget d2dc, Geometry geometry, Brush brush, float strokeWidth = 1)
+        public static DeviceContext ChainDrawGeometry(this DeviceContext d2dc, Geometry geometry, Brush brush, float strokeWidth = 1)
         { d2dc.DrawGeometry(geometry, brush, strokeWidth); return d2dc; }
-        public static RenderTarget ChainDrawGeometry(this RenderTarget d2dc, Geometry geometry, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
+        public static DeviceContext ChainDrawGeometry(this DeviceContext d2dc, Geometry geometry, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
         { d2dc.DrawGeometry(geometry, brush, strokeWidth, strokeStyle); return d2dc; }
-        public static RenderTarget ChainDrawGlyphRun(this RenderTarget d2dc, RawVector2 baselineOrigin,
+        public static DeviceContext ChainDrawGlyphRun(this DeviceContext d2dc, RawVector2 baselineOrigin,
             GlyphRun glyphRun, Brush foregroundBrush, MeasuringMode measuringMode = MeasuringMode.Natural)
         { d2dc.DrawGlyphRun(baselineOrigin, glyphRun, foregroundBrush, measuringMode); return d2dc; }
         public static DeviceContext ChainDrawGlyphRun(this DeviceContext d2dc, RawVector2 baselineOrigin,
             GlyphRun glyphRun, GlyphRunDescription glyphRunDescription, Brush foregroundBrush, MeasuringMode measuringMode = MeasuringMode.Natural)
         { d2dc.DrawGlyphRun(baselineOrigin, glyphRun, glyphRunDescription, foregroundBrush, measuringMode); return d2dc; }
-        public static RenderTarget ChainDrawLine(this RenderTarget d2dc, RawVector2 point0, RawVector2 point1, Brush brush, float strokeWidth = 1)
+        public static DeviceContext ChainDrawLine(this DeviceContext d2dc, RawVector2 point0, RawVector2 point1, Brush brush, float strokeWidth = 1)
         { d2dc.DrawLine(point0, point1, brush, strokeWidth); return d2dc; }
-        public static RenderTarget ChainDrawLine(this RenderTarget d2dc, RawVector2 point0, RawVector2 point1, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
+        public static DeviceContext ChainDrawLine(this DeviceContext d2dc, RawVector2 point0, RawVector2 point1, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
         { d2dc.DrawLine(point0, point1, brush, strokeWidth, strokeStyle); return d2dc; }
-        public static RenderTarget ChainDrawRectangle(this RenderTarget d2dc, RawRectangleF rect, Brush brush, float strokeWidth = 1)
+        public static DeviceContext ChainDrawRectangle(this DeviceContext d2dc, RawRectangleF rect, Brush brush, float strokeWidth = 1)
         { d2dc.DrawRectangle(rect, brush, strokeWidth); return d2dc; }
-        public static RenderTarget ChainDrawRectangle(this RenderTarget d2dc, RawRectangleF rect, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
+        public static DeviceContext ChainDrawRectangle(this DeviceContext d2dc, RawRectangleF rect, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
         { d2dc.DrawRectangle(rect, brush, strokeWidth, strokeStyle); return d2dc; }
-        public static RenderTarget ChainFillEllipse(this RenderTarget d2dc, Ellipse ellipse, Brush brush) { d2dc.FillEllipse(ellipse, brush); return d2dc; }
-        public static RenderTarget ChainFillGeometry(this RenderTarget d2dc, Geometry geometry, Brush brush) { d2dc.FillGeometry(geometry, brush); return d2dc; }
-        public static RenderTarget ChainFillMesh(this RenderTarget d2dc, Mesh mesh, Brush brush) { d2dc.FillMesh(mesh, brush); return d2dc; }
-        public static RenderTarget ChainFillRectangle(this RenderTarget d2dc, RawRectangleF rect, Brush brush) { d2dc.FillRectangle(rect, brush); return d2dc; }
-        public static RenderTarget ChainFillRoundedRect(this RenderTarget d2dc, RoundedRectangle rect, Brush brush) { d2dc.FillRoundedRectangle(rect, brush); return d2dc; }
-        public static RenderTarget ChainDrawRoundedRect(this RenderTarget d2dc, RoundedRectangle roundedRect, Brush brush, float strokeWidth = 1)
+        public static DeviceContext ChainFillEllipse(this DeviceContext d2dc, Ellipse ellipse, Brush brush) { d2dc.FillEllipse(ellipse, brush); return d2dc; }
+        public static DeviceContext ChainFillGeometry(this DeviceContext d2dc, Geometry geometry, Brush brush) { d2dc.FillGeometry(geometry, brush); return d2dc; }
+        public static DeviceContext ChainFillMesh(this DeviceContext d2dc, Mesh mesh, Brush brush) { d2dc.FillMesh(mesh, brush); return d2dc; }
+        public static DeviceContext ChainFillRectangle(this DeviceContext d2dc, RawRectangleF rect, Brush brush) { d2dc.FillRectangle(rect, brush); return d2dc; }
+        public static DeviceContext ChainFillRoundedRect(this DeviceContext d2dc, RoundedRectangle rect, Brush brush) { d2dc.FillRoundedRectangle(rect, brush); return d2dc; }
+        public static DeviceContext ChainDrawRoundedRect(this DeviceContext d2dc, RoundedRectangle roundedRect, Brush brush, float strokeWidth = 1)
         { d2dc.DrawRoundedRectangle(roundedRect, brush, strokeWidth); return d2dc; }
-        public static RenderTarget ChainDrawRoundedRect(this RenderTarget d2dc, RoundedRectangle roundedRect, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
+        public static DeviceContext ChainDrawRoundedRect(this DeviceContext d2dc, RoundedRectangle roundedRect, Brush brush, StrokeStyle strokeStyle, float strokeWidth = 1)
         { d2dc.DrawRoundedRectangle(roundedRect, brush, strokeWidth, strokeStyle); return d2dc; }
-        public static RenderTarget ChainDrawText(this RenderTarget d2dc, string text, TextFormat textFormat, RawRectangleF layoutRect,
+        public static DeviceContext ChainDrawText(this DeviceContext d2dc, string text, TextFormat textFormat, RawRectangleF layoutRect,
             Brush defaultForegroundBrush, DrawTextOptions options = DrawTextOptions.None, MeasuringMode measuringMode = MeasuringMode.Natural)
         { d2dc.DrawText(text, textFormat, layoutRect, defaultForegroundBrush, options, measuringMode); return d2dc; }
-        public static RenderTarget ChainDrawTextLayout(this RenderTarget d2dc, RawVector2 origin,
+        public static DeviceContext ChainDrawTextLayout(this DeviceContext d2dc, RawVector2 origin,
             TextLayout textLayout, Brush defaultFillBrush, DrawTextOptions options = DrawTextOptions.None)
         { d2dc.DrawTextLayout(origin, textLayout, defaultFillBrush, options); return d2dc; }
         public static DeviceContext ChainDrawGdiMetafile(this DeviceContext d2dc, GdiMetafile gdiMetafile, RawVector2? targetOffset)
