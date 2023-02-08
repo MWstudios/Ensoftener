c27ac123d24a0e83cf458e49111fcff5caa34bdc
MWstudios <mwstudios@users.noreply.github.com>
2023-02-08 16:31:47 +0100

    changed Task to Thread

--- src/Global.cs
+++ src/Global.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
@@ -8,17 +7,19 @@ namespace Ensoftener
     /// <summary>Extensions, Win32 calls and common methods shared across DirectX and Vulkan.</summary>
     public static class GShared
     {
-        static uint renderThreadId = 0; internal static bool Quitting { get; private set; }
+        internal static uint MainThreadId = 0; internal static bool Quitting { get; private set; }
+        public static uint SystemVolume
+        {
+            get { _ = waveOutGetVolume(IntPtr.Zero, out uint CurrVol); return (CurrVol & 0x0000ffff) * 100 / ushort.MaxValue; }
+            set { uint NewVolume = ushort.MaxValue * Math.Min(value, 100) / 100; _ = waveOutSetVolume(IntPtr.Zero, (NewVolume & 0x0000ffff) | (NewVolume << 16)); }
+        }
         public static List<SideLoop> SideLoops { get; } = new();
-        static GShared() { renderThreadId = GetCurrentThreadId(); }
+        static GShared() { MainThreadId = GetCurrentThreadId(); }
         public static event EventHandler OnQuit;
-        /// <summary>A loop separated from the main rendering method. The loop will run ONLY after <b><see cref="GDX.Run(Action, bool)"/></b> has started.</summary>
+        /// <summary>A loop separated from the main rendering method.</summary>
         public class SideLoop
         {
-            bool nameUpdated = false; string name;
-            /// <summary>The name of the thread that will appear on the debugger.</summary>
-            public string Name { get => name; set { name = value; nameUpdated = true; } }
-            public System.Threading.Tasks.Task Task { get; }
+            public System.Threading.Thread Thread { get; }
             /// <summary>The loop's refresh rate, in miliseconds. Accepts up to 4 decimal numbers. Default is 16.6667 ms.</summary>
             public double RefreshRate { get; set; } = 16.6667;
             /// <summary>Updates mouse+keyboard input in this thread. This is very important to look after, as updating input while another thread
@@ -29,25 +30,30 @@ namespace Ensoftener
             public SideLoop(Action updateMethod)
             {
                 UpdateMethod = updateMethod;
-                Task = System.Threading.Tasks.Task.Run(() =>
+                Thread = new(() =>
                 {
                     System.Diagnostics.Stopwatch t = new(); t.Start();
-                    while (renderThreadId == 0) ; AttachThreadInput(GetCurrentThreadId(), renderThreadId, true);
+                    while (MainThreadId == 0) ; AttachThreadInput(GetCurrentThreadId(), MainThreadId, true);
                     while (!Quitting)
                     {
-                        while (t.ElapsedTicks < RefreshRate * 10000) ; if (nameUpdated && Name != null) Thread.CurrentThread.Name = Name;
+                        while (t.ElapsedTicks < RefreshRate * 10000) ;
                         UpdateMethod(); if (UpdateInput) Input.Input.Update(); t.Restart();
                     }
                 });
+                Thread.Start();
             }
         }
+        [DllImport("winmm.dll")] static extern int waveOutGetVolume(IntPtr hwo, out uint dwVolume);
+        [DllImport("winmm.dll")] static extern int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+        [DllImport("user32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        internal static extern long SendMessage(IntPtr hwnd, uint message, IntPtr wParam, IntPtr lParam);
         [DllImport("user32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
         internal static extern bool AttachThreadInput(uint source, uint dest, bool attach);
         [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)] internal static extern uint GetCurrentThreadId();
         /// <summary>Schedules a shutdown of the window. After the current rendering method ends, all sounds are stopped and components deleted.</summary>
         /// <remarks>Windows doesn't allow for different threads to close a single window, which means the closure needs to be scheduled for later
         /// and then executed by the window thread.</remarks>
-        public static void Quit() { Quitting = true; OnQuit(null, null); }
+        public static void Quit() { Quitting = true; OnQuit?.Invoke(null, null); }
         #region extensions
         /// <summary>Adds an object to the end of the <seealso cref="List{T}"/> only if the object isn't already present.</summary>
         public static void AddIfMissing<T>(this List<T> list, T item) { if (!list.Contains(item)) list.Add(item); }
